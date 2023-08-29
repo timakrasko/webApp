953af0d7c42f2e1c18e178d3e12cd332c77d4f65
Vladislav Kostiuk <vladis.dumanchuk@gmail.com>
2023-08-29 17:59:13 +0300

    Modified WebSecurityConfig

--- src/main/java/com/project/webApp/config/WebSecurityConfig.java
+++ src/main/java/com/project/webApp/config/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.project.webApp.config;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,11 +17,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/", "/register", "/users", "/confirm").permitAll()
-                        .requestMatchers("/*", "/users/*", "/films/*").permitAll()
+        http.authorizeHttpRequests(requests -> requests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/*", "/users/*", "/films/*", "/img/**").permitAll()
                         .requestMatchers("/users/*/block", "/users/*/unblock", "/films/*/edit", "/films/*/delete").hasAuthority("ADMIN")
-//                        .requestMatchers("/*", "/users/*", "/films/*").hasAnyAuthority("ADMIN", "USER")
                         .anyRequest().authenticated()
                 );
         http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
@@ -25,6 +28,7 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
 }
 
 
